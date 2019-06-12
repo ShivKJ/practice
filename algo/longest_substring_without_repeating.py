06082763a67687a26d1a773dbd9e9bfa0e02b2d8
ShivKJ <shivkj001@gmail.com>
2019-06-12 22:40:35 +0530

    fixing bug

--- algo/longest_substring_without_repeating.py
+++ algo/longest_substring_without_repeating.py
@@ -1,18 +1,34 @@
 def fun(string: str) -> str:
+    if not string:
+        return 0
+
     s, e = 0, 0
     start_idx, end_idx = 0, 0
 
+    characters = [None] * 128
+    characters[ord(string[0])] = 0
+
     for i in range(1, len(string)):
-        if string[i] == string[end_idx]:
+        key = ord(string[i])
+        idx = characters[key]
+
+        if idx is not None and idx >= start_idx:
             if e - s < end_idx - start_idx:
                 s, e = start_idx, end_idx
 
-            start_idx = end_idx = i
-        else:
-            end_idx = i
+            start_idx = idx + 1
 
-    return string[s:e + 1]
+        end_idx = i
+
+        characters[key] = i
+    else:
+        if e - s < end_idx - start_idx:
+            s, e = start_idx, end_idx
+    return e - s + 1
 
 
 if __name__ == '__main__':
-    print(fun('aabccccdefggg'))
+    # print(fun('aabccccdefggg'))
+    # print(fun("abcabcbb"))
+    # print(fun('au'))
+    print(fun("dvdf"))
