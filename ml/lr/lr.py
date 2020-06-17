377f0dda69676e25d9c2bbc08a944c7dd9146961
Shiv Krishna Jaiswal <shivkrishna.jaiswal@delhivery.com>
2020-06-17 09:48:21 +0530

    updating code with respect to tensor flow v2.0

--- ml/lr/lr.py
+++ ml/lr/lr.py
@@ -1,49 +1,38 @@
+import pandas as pd
 import tensorflow as tf
-from numpy import array, linspace
-from numpy.random import random
-from streamAPI.utility import csv_itr
-
-
-def generate_batch(n: int):
-    x_batch = linspace(-1, 1, n)
-    y_batch = 2 * x_batch + random(x_batch.shape) * 0.3
-    return x_batch, y_batch
 
 
 def vehicle_data():
-    itr = csv_itr('data/refined_mile_per_gallon.csv')
-    data = []
+    df = pd.read_csv('refined_mile_per_gallon.csv')
+    df = df[[
+        'cylinders',
+        'displacement',
+        'horsepower',
+        'weight',
+        'acceleration',
+        'mpg'
+    ]]
 
-    for doc in itr:
-        data.append((
-            int(doc['cylinders']),
-            float(doc['displacement']),
-            int(doc['horsepower']),
-            int(doc['weight']),
-            float(doc['acceleration']),
-            float(doc['mpg'])
-        ))
-
-    data = array(data)
+    data = df.values
     size = 200
-    x_train, y_train = data[:200, :-1], data[:200, -1].reshape((size, 1))
-    x_test, y_test = data[200:, :-1], data[200:, -1].reshape((len(data) - size, 1))
+    x_train, y_train = data[:size, :-1], data[:size, -1].reshape((size, 1))
+    x_test, y_test = data[size:, :-1], data[size:, -1].reshape((len(data) - size, 1))
 
     return x_train, y_train, x_test, y_test
 
 
 def linear_regression(alpha):
-    with tf.name_scope('Graph') as params:
+    with tf.name_scope('Graph'):
         feature_size = 5
         output_size = 1
 
-        x = tf.placeholder(dtype=tf.float64, shape=[None, feature_size], name='x')
-        y = tf.placeholder(dtype=tf.float64, shape=[None, output_size], name='y')
+        x = tf.compat.v1.placeholder(dtype=tf.float64, shape=[None, feature_size], name='x')
+        y = tf.compat.v1.placeholder(dtype=tf.float64, shape=[None, output_size], name='y')
 
-        w = tf.Variable(tf.random_normal([feature_size, output_size], dtype=tf.float64),
+        w = tf.Variable(tf.compat.v1.random_normal([feature_size, output_size], dtype=tf.float64),
                         name='W', dtype=tf.float64)
 
-        b = tf.Variable(tf.random_normal([output_size], dtype=tf.float64),
+        b = tf.Variable(tf.compat.v1.random_normal([output_size], dtype=tf.float64),
                         name='b', dtype=tf.float64)
 
         y_pred = x @ w + b
@@ -61,23 +50,23 @@ def run():
 
     learning = 0.1
 
-    optimizer = tf.train.AdamOptimizer(learning_rate=learning).minimize(cost)
-    init = tf.global_variables_initializer()
+    optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=learning).minimize(cost)
+    init = tf.compat.v1.global_variables_initializer()
 
-    writer = tf.summary.FileWriter('./log', graph=tf.get_default_graph())
+    tf.compat.v1.summary.FileWriter('./log', graph=tf.compat.v1.get_default_graph())
 
-    with tf.Session() as sess:
+    with tf.compat.v1.Session() as sess:
         sess.run(init)
 
         for epoch in range(50000):
             _, batch_cost = sess.run([optimizer, cost], feed_dict={x: x_batch, y: y_batch})
-            print(epoch, batch_cost)
+
         y_t = sess.run(cost, {x: x_test, y: y_test})
 
         print(y_t)
 
 
 if __name__ == '__main__':
-    # vehicle_data()
-    # linear_regression()
+    tf.compat.v1.disable_eager_execution()
+
     run()
