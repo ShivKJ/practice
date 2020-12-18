2ae943f1204bb5305864a1e46e9ebc8c24d0c056
Shiv Krishna Jaiswal <shivkrishna.jaiswal@delhivery.com>
2020-12-18 19:15:27 +0530

    updating doc

--- leetcodeOthers/problem46.py
+++ leetcodeOthers/problem46.py
@@ -1,12 +1,30 @@
 from typing import List
 
 
-def permutation(index, values):
+def permutation(index: int, values: List[int]):
+    """
+    creating recursive function to get all the permutation
+    of elements from values
+    Example:
+    A) if values = [1, 2, 3] then output
+        1) [1, 2, 3]
+        2) [2, 1, 3]
+        3) [2, 3, 1]
+        4) [1, 3, 2]
+        5) [3, 1, 2]
+        6) [3, 2, 1]
+    B) if values = [1], the output
+        1) [1]
+    number of elements in output = n!
+                                 = n * (n-1)! where 0! = 1
+    :param index:
+    :param values:
+    """
     n = len(values)
     v = values[index]
 
     if index == n - 1:
-        yield from [[v]]
+        yield from [[v]]  # only on element in output
     else:
         for e in permutation(index + 1, values):
             for j in range(index, n):
